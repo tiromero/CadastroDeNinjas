319cc82cc0baae6542b3ebbb7fb4f50a893dbf7d
tiromero <72394348+tiromero@users.noreply.github.com>
2026-07-20 18:00:58 -0300

    `FEAT: `Cria uma entidade no BD apartir do NinjaModel.

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,14 +1,20 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity //Entity ele transforma uma Classe comum em uma entidade do Banco de Dados (tabela)
+@Table( name = "tb_cadastro")
 @NoArgsConstructor //criando construtor sem argumentos
 @AllArgsConstructor // criando construtores com argumentos
 @Data //criando getters e setters
 public class NinjaModel {
+    @Id //Pegar o atributo abaixo (Long id) e transformar em ID na tabela
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //organizar estregicamente por id
+    private Long id;
+
     private String nome;
 
     @Column(unique = true) //Criando um email unico para 1 usario
