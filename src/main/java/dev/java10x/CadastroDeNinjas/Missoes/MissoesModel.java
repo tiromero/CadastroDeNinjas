4c0d96b5abadef4745788c5217f9b2a1c7c8819a
tiromero <72394348+tiromero@users.noreply.github.com>
2026-07-21 13:05:23 -0300

    Modelagem do BD de NinjaModel e MissoesModel, `anotations` para vincular tabelas.

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,14 +1,25 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import dev.java10x.CadastroDeNinjas.Ninjas.NinjaModel;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
+@Table(name = "tb_missoes")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class MissoesModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long idMissoes;
+
     private String nome;
     private String descricao;
     private DificuldadeRank dificuldade;
+
+    @OneToMany(mappedBy = "missoes") //Uma missao para varios ninjas //mapped é mapiar a COLUNA que estou usando la em NinjaMOdel
+    private NinjaModel ninja;
 }
--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,9 +1,12 @@
 package dev.java10x.CadastroDeNinjas.Ninjas;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity //Entity ele transforma uma Classe comum em uma entidade do Banco de Dados (tabela)
 @Table( name = "tb_cadastro")
@@ -20,5 +23,9 @@ public class NinjaModel {
     @Column(unique = true) //Criando um email unico para 1 usario
     private String email;
     private int idade;
+
+    @ManyToOne //Serve para que 1 ninja tenha uma unica missao
+    @JoinColumn(name = "missoes_id") //Serve para juntar tabela Ninja e Missoes //Foreing Key ou chave estrangeira que cria uma coluna de missoes_id que linka as duas tabelas.
+    private MissoesModel missoes;
 
 }
